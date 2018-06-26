d04d4281c72b6a98258143a61a2c2f1458a087c0
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-06-26 11:38:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,28 +13,7 @@ const status = {
 };
 
 
-const music = require('discord.js-musicbot-addon');
 
-
-music.start(client, {
-  youtubeKey: "AIzaSyAAKSiOkapv22Kt3OWNOAWeTcXJ0yLmJ90",
-  prefix: "-",
-  leaveAlt: ["fuckoff", "getout"],
-  helpCmd: "mhelp",
-  thumbnailType: "high",
-  maxQueueSize: 5,
-  enableQueueStat: true,
-  ownerOverMember: true,
-  clearOnLeave: true,
-  anyoneCanSkip: true,
-  anyoneCanPause: true,
-  logging: true,
-  requesterName: true,
-  embedColor: "RANDOM",
-  checkQueues: true,
-  pauseAlt: ['stop', 'wait'],
-  botOwner: "368756694114893825"
-});
 
 
 client.on('guildMemberAdd', member => {
